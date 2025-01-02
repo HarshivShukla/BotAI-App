f73868434549c45aa7ebd2628bdfc9c0b0dfe8f4
HarshivShukla <harshivshukla46@gmail.com>
2025-01-02 21:35:23 +0530

    Past Conversation update

--- botai/src/components/ConversationList.js
+++ botai/src/components/ConversationList.js
@@ -6,8 +6,19 @@ const ConversationList = ({ conversations, onSelectConversation }) => {
       <h3>Past Conversations</h3>
       <ul>
         {conversations.map((conv, index) => (
-          <li key={index} onClick={() => onSelectConversation(index)}>
-            Conversation {index + 1}
+          <li
+            key={index}
+            className="conversation-item"
+            onClick={() => onSelectConversation(index)}
+          >
+            <div className="conversation-header">
+              <h4>Conversation {index + 1}</h4>
+              {/* Add more details like date or feedback if necessary */}
+            </div>
+            <div className="conversation-preview">
+              {/* Display the first message of each conversation as a preview */}
+              {conv[0] && <p>{conv[0].text}</p>}
+            </div>
           </li>
         ))}
       </ul>
